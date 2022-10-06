c092a55c88c6dd4eb79a9d565578c6bbd03e59ea
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-10-06 18:00:26 +0200

    fdfdf

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-import "./style/style.css";
-// Hamburger Menu
-function Menu(e){
-    let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
-  }
+import "../style/style.css";
+
+
+const contacForm = document.querySelector("#contact-form");
+console.log(contacForm);

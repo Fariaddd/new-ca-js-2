a9cdaa8a265a2f94480619fa58c369e26930173a
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-10-06 23:25:26 +0200

    Form validation

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ import '../style/style.css'
 const contacForm = document.querySelector("#contact-form");
 const firstName = document.querySelector("#firstName");
 const firstNameError = document.querySelector("#firstNameError");
+const email = document.querySelector("#email");
+const emailErrorNotValid = document.querySelector("#emailErrorNotValid");
+const emailError = document.querySelector("#emailError");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError");
+const passwordErrorNotValid = document.querySelector("#passwordErrorNotValid");
+const confirmPassword = document.querySelector("#confirm_password");
+const confirmPasswordErrorNotMatching = document.querySelector("#confirmPasswordErrorNotMatching");
+const confirmPasswordError = document.querySelector("#confirmPasswordError");
+
+
+
 
 
 contacForm.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    let isFirstName = false;
+    
+    
 
     
 

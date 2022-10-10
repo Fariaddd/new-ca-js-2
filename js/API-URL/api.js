6e9e7424124aa13bba8096d913ba47def9c1baa6
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-10-10 22:48:27 +0200

    Edit

--- js/API-URL/api.js
+++ js/API-URL/api.js
@@ -6,6 +6,7 @@ const API_BASE_URL = "https://nf-api.onrender.com/"
 const USER_LOGIN = API_BASE_URL + "api/v1/social/auth/login"
 const USER_SIGNUP = API_BASE_URL + "api/v1/social/auth/register"
 
+const GET_POST_BY_ID = API_BASE_URL + "api/v1/social/posts"
 
 const CREATE_POST = API_BASE_URL + "api/v1/social/posts"
 const GET_POSTS_URL = API_BASE_URL + "api/v1/social/posts"
@@ -13,7 +14,7 @@ const GET_USER_POSTS = API_BASE_URL + `api/v1/social/profiles/${userName}?_posts
 const DELETE_POST_BY_ID = API_BASE_URL + `api/v1/social/posts`
 
 
-export {API_BASE_URL, USER_LOGIN, USER_SIGNUP, CREATE_POST, GET_POSTS_URL, GET_USER_POSTS, DELETE_POST_BY_ID};
+export {API_BASE_URL, USER_LOGIN, USER_SIGNUP, CREATE_POST, GET_POSTS_URL, GET_USER_POSTS, DELETE_POST_BY_ID, GET_POST_BY_ID};
 
 
 
---
+++ js/getpostbyid.js
@@ -0,0 +1,46 @@
+import { getToken } from "./Storage/Storage.js";
+import {GET_POST_BY_ID} from "./API-URL/api.js";
+
+const paramString = window.location.search;
+const searchParam = new URLSearchParams(paramString);
+const postId = searchParam.get("post_id");
+const singlePostContainer = document.getElementById("post-details");
+const accessToken = getToken();
+
+async function getPostById (){
+    const response = await fetch(`${GET_POST_BY_ID}/${postId}`,{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        }
+    })
+    const data = await response.json();
+    
+    const {title, body, updated, id} = data;
+    singlePostContainer.innerHTML = `
+                <dl>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500 capitalize">title</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${title}
+                        </dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500 capitalize">description</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${body}</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500 capitalize">id</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${id}</dd>
+                    </div>
+                 
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500 capitalize">updated</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${updated}</dd>
+                    </div>
+                </dl>
+    `
+
+    
+};
+getPostById();

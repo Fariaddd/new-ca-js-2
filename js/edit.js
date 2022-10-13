c068a440d85bb493c59b3e99907f5bd89790ff31
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-10-13 23:54:57 +0200

    added Edit.js & html file with its id and validation

--- js/edit.js
+++ js/edit.js
@@ -1,14 +1,19 @@
 import { getToken } from "./Storage/Storage";
 
-const postTitle = document.querySelector("#postTitle")
-const postDescription = document.querySelector("#postDescription")
-const editPostForm = document.querySelector("#edit-post-form")
+const postTitle = document.querySelector("#postTitle");
+const postDescription = document.querySelector("#postDescription");
+const editPostForm = document.querySelector("#edit-post-form");
+const postTitleError = document.querySelector("#postTitleError");
+const postDescriptionError = document.querySelector("#postDescriptionError");
 
 const paramString = window.location.search;
 const searchParam = new URLSearchParams(paramString);
 const postId = searchParam.get("post_id");
 
 const accessToken = getToken();
+if(!accessToken){
+    location.href = "/sign-in.html" 
+}
 
 async function getPostByIdDynamic (){
     const response = await fetch (`https://nf-api.onrender.com/api/v1/social/posts/${postId}`, {
@@ -32,7 +37,22 @@ editPostForm.addEventListener("submit", function (event){
         "title": postTitle.value,
         "body": postDescription.value
     }
-    console.log("postData", postData);
+    async function editPost (){
+        const response = await fetch (`https://nf-api.onrender.com/api/v1/social/posts/${postId}`, {
+            method : "PUT",
+            headers : { 
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            },
+            body: JSON.stringify(postData)
+            })
+            if(response.ok){
+                location.href = `single-post.html?post_id=${postId}`
+            }
+
+        
+    }
+    editPost();
 })
 
 

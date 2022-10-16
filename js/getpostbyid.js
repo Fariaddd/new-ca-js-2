84b4be8c29b3f5a2015beebb67b90a1eb1279dd9
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-10-16 22:11:50 +0200

    CA-JS-2

--- js/getpostbyid.js
+++ js/getpostbyid.js
@@ -22,29 +22,23 @@ async function getPostById (){
     const {title, body,  updated, id, created} = data; 
 
     
-    singlePostContainer.innerHTML = `   <dl>
-    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500 capitalize">title</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${title}
-        </dd>
-    </div>
-    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500 capitalize">description</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${body}</dd>
-    </div>
-    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500 capitalize">id</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${id}</dd>
-    </div>
-    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500 capitalize">created</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${created}</dd>
-    </div>
-    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500 capitalize">updated</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">${updated}</dd>
-    </div>
-</dl>` 
+    singlePostContainer.innerHTML = `<dl>
+                                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dd class="mt-1 pl-12 font-bold text-gray-900 sm:col-span-2 sm:mt-0">${title} </dd>
+                                    </div>
+                                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dd class="mt-1 pl-12  font-bold text-gray-900 sm:col-span-2 sm:mt-0">${body}</dd>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-5  sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dd class="mt-1 pl-12  font-bold text-gray-900 sm:col-span-2 sm:mt-0">${id}</dd>
+                                    </div>
+                                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dd class="mt-1 pl-12  font-bold text-gray-900 sm:col-span-2 sm:mt-0">${created}</dd>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dd class="mt-1 pl-12  font-bold text-gray-900 sm:col-span-2 sm:mt-0">${updated}</dd>
+                                    </div>
+                                    </dl>` 
 }
 getPostById();
 
